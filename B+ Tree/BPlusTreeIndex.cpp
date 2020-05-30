f994b3f4091af97e3e7a40cd3d619b34ad732f95
jdavalos1 <43660774+jdavalos1@users.noreply.github.com>
2020-05-29 20:51:41 -0700

    Adding search functionality
    Adding search function which is recursive.

--- B+ Tree/BPlusTreeIndex.cpp
+++ B+ Tree/BPlusTreeIndex.cpp
@@ -1,19 +1,78 @@
 #include "BPlusTreeIndex.h"
 using namespace BPTree;
 
-BPTreeIndex::BPTreeIndex(pair<char, char>* data, bool isLeaf, int fanout, float fill) : _isLeaf(isLeaf), _fanout(fanout), _fill_factor(fill), _data(data) {}
-
-void BPTreeIndex::insert(pair<char, char> *data)
+void BPTreeIndex::insert(pair<int, int> *data)
 {
 
 }
 
-void BPTreeIndex::search(char data)
+list<pair<int, int>>* BPTreeIndex::search(const int data)
 {
-
+    return recursive_search(this, data);
 }
 
-void bulk_load(list<pair<char, char> data)
+/**
+ * @brief 
+ * Recurses through tree to find a node with the needed value
+ * as well as all the duplicates found
+ * @param node Node to begin search
+ * @param data Data entry to search
+ * @return list<pair<int, int>>* List of <record id, data value>
+ */
+list<pair<int, int>>* BPTreeIndex::recursive_search(BPTreeIndex *node, const int& data)
+{
+    // Base case if the tree index is a leaf then we can
+    // obtain all the values from the node
+    if(node->_isLeaf)
+    {
+        auto data_entries = new list<pair<int,int>>();
+        bool duplicatesExists = true;
+        auto current = node;
+        
+        // Duplicates can exist and thus the easiest way to obtain them is to
+        // search first for the left most leaf then traverse to the right
+        // on the leaves and fill a list to return.
+        while(duplicatesExists)
+        {
+            for(auto it = node->_data->begin(); it != node->_data->end(); it++)
+            {
+                if(it->second == data)
+                {
+                    data_entries->push_back(pair<int,int>(it->first, it->second));
+                }
+                else
+                {
+                    // The last duplicate was found
+                    duplicatesExists = false;
+                    break;
+                }
+            }
+            current = current->_neighbor;
+            // Are there any neighboring leaves which can contain duplicates?
+            if(current == NULL) duplicatesExists = false;
+       }
+       return data_entries;
+    }
+    
+    // Search for the index which the data is less than the value
+    // i.e. intervals -> [4, 6, 8] then 2 will be < intervals[0]
+    // and 5 < intervals[1] but > intervals[0] in this for loop
+    // Note: 4 will not be < intervals[0]
+    // but between [intervals[0], intervals[1])
+    int interval;
+    for(auto i = 0; i < node->_intervals->size(); i++)
+    {
+        if(data < node->_intervals->at(i))
+        {
+            interval = i;
+            break;
+        }
+    }
+    // Recurse down to find the value needed based on index found
+    return recursive_search(node->_children->at(interval), data);
+}
+
+void bulk_load(list<pair<int, int>> data)
 {
 
 }
--- B+ Tree/BPlusTreeIndex.h
+++ B+ Tree/BPlusTreeIndex.h
@@ -1,23 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <list>
 using namespace std;
 
 namespace BPTree {
-
     class BPTreeIndex
     {
         private:
-            pair<char, char> *_data;
-            list<BPTreeIndex*> _children;
-            bool _isLeaf;
             int _fanout;
-            float _fill_factor;
+            bool _isLeaf;
+            list<pair<int, int>> *_data;
+            vector<int>* _intervals;
+            BPTreeIndex *parent;
+            vector<BPTreeIndex*> *_children;
+            BPTreeIndex *_neighbor;
+            // Helper functions
+            list<pair<int, int>>* recursive_search(BPTreeIndex*, const int&);
         
         public:
-            BPTreeIndex(pair<char, char>*, bool, int, float);
-            void insert(pair<char, char>);
-            void search(char);
-            void bulk_load(list<pair<char, char>>);
-
+            BPTreeIndex(pair<int, int>*, bool, int, float);
+            void insert(pair<int, int>*);
+            list<pair<int, int>>* search(const int);
+            void bulk_load(list<pair<int, int>>);
     };
 }

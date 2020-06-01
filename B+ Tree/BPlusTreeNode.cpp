0ae4087de6f16ce5c604ca9f205e56decb300aac
jdavalos1 <43660774+jdavalos1@users.noreply.github.com>
2020-05-31 21:02:12 -0700

    Insert function completed
    Testing is now required on insert and will be done after persistance addition. Temp commit before tests

--- B+ Tree/BPlusTreeNode.cpp
+++ B+ Tree/BPlusTreeNode.cpp
@@ -15,14 +15,25 @@ list<pair<int, int>>* BPTreeManager::search(const int data)
     return root == NULL ? NULL : root->recursive_search(data);
 }
 
+/**
+ * @brief Insert a data record into the tree
+ * 
+ * @param record New data record to be inserted into
+ * a tree
+ */
 void BPTreeManager::insert(pair<int, int>* record)
 {
+    // If the root is null then we need to create
+    // a new root
     if(root == NULL)
     {
         root = new BPTreeNode(true, fanout);
     }
+    // Call recursive insert on the new data record
     BPTreeNode* newRoot = root->recursive_insert(record);
     
+    // If the value returned by newRoot is not NULL 
+    // then the root was split and we have a new root
     if(newRoot != NULL) root = newRoot;
 }
 
@@ -88,39 +99,65 @@ list<pair<int, int>>* BPTreeNode::recursive_search(const int& searchKey)
 }
 
 
+/**
+ * @brief Recursively step into the tree to insert a value.
+ * If the insertion exceeds the fanout-1 space in a leaf
+ * this function will recursively split the tree and return
+ * a new root if needed.
+ * 
+ * @param data_record Data record being added to a page/leaf
+ * @return BPTreeNode* A potential new root to the tree after
+ * a split is completed.
+ */
 BPTreeNode* BPTreeNode::recursive_insert(const pair<int, int>* data_record)
 {
+    // Check if the node is a leaf and therefore a page to add values
+    // Otherwise we continue to search
     if(this->_isLeaf)
     {
+        // Find the location where the new data record can be added
+        // to the page/leaf
         for(auto it = this->_data->begin(); it != this->_data->end(); it++)
         {
+            // Add the data in a sorted order in the page
             if(it->first < data_record->first)
             {
                 this->_data->insert(it, *data_record);
                 break;
             }
         }
+        // Check if this caused an overflow
+        // thus requring a split on the leaf to accomadate
+        // for the size constraints
         if(this->_data->size() >= this->_fanout-1)
         {
-            return recursive_split();
+            return this->recursive_split();
         }
     }
     else
     {
-        int interval;
-
-        for(auto i = 0; i < this->_intervals->size(); i++)
+        // The node is not a leaf and a search must continue
+        int i;
+        for(i = 0; i < this->_intervals->size(); i++)
         {
+            // Check if the data records search key is less than
+            // the interval at i in order to break and add the value
+            // to that location.
             if(data_record->first < this->_intervals->at(i))
-            {
-                interval = i;
                 break;
-            }
         }
-        return this->_children->at(interval)->recursive_insert(data_record);
+        // Call a recursive insert to find out where we can place
+        // the data record
+        return this->_children->at(i)->recursive_insert(data_record);
     }
 }
-
+/**
+ * @brief Recursively splits the tree at every node
+ * until a node no longer needs to split.
+ * 
+ * @return BPTreeNode* Return a potential new root if the
+ * root needs to be split
+ */
 BPTreeNode* BPTreeNode::recursive_split()
 {
     // A leaf node has been found which must be split
@@ -154,7 +191,7 @@ BPTreeNode* BPTreeNode::recursive_split()
     }
     else
     {
-        if(this->_parent == NULL)
+        if(this->_intervals->size() >= this->_fanout - 1)
         {
             // Find position of split and it value
             auto splitPos = this->_intervals->size() / 2;
@@ -167,14 +204,32 @@ BPTreeNode* BPTreeNode::recursive_split()
             // remove the value from the first node since they belong to the second set
             this->_intervals->erase(this->_intervals->begin() + splitPos, this->_intervals->end());
             this->_children->erase(this->_children->begin() + (splitPos+1), this->_children->end());
-            // create new node as the parent (root)
-            auto newParent = new BPTreeNode(false, this->_fanout,
-                                            new vector<BPTreeNode*>(this, newNode), new vector<int>(parentInterval));
+            if(this->_parent == NULL)
+            {
+                // create new node as the parent (root)
+                return new BPTreeNode(false, this->_fanout,
+                                                new vector<BPTreeNode*>(this, newNode), new vector<int>(parentInterval));
+            }
+            else
+            {
+                // add the new interval and child to the list of nodes
+                // call recursive split to see if we added too many
+                // intervals to the parent node
+                this->_parent->add_child(parentInterval, newNode);
+                return this->_parent->recursive_split();
+            }
         }
     }
-    
+    return NULL;
 }
 
+/**
+ * @brief Adds a child node to the parent node calling the function
+ * 
+ * @param leftmostValue Newest interval value of child to add to
+ * parent intervals
+ * @param child Newest child to add to list of children
+ */
 void BPTreeNode::add_child(int leftmostValue, BPTreeNode* child)
 {
     // Locate where the new value can be added to the intervals
